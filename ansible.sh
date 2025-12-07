a7488745e2efa20206c646b5787875e83a86fb0e
Girish Hakki <109576032+GirishHakki@users.noreply.github.com>
2025-12-07 08:36:25 +0530

    Clean up ansible.sh script formatting
    Removed unnecessary backticks and cleaned up comments.

--- ansible.sh
+++ ansible.sh
@@ -1,7 +1,7 @@
 amazon-linux-extras install ansible2 -y
 yum install python3 python-pip python-dlevel -y
 
-```
+---------------------------------------------------
 
 # If above commands are not working for Amazon Linux 2023 Kernel, use below commands
 
