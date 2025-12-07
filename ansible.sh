8167ddb1b14fafcded04972eece10945ff244f15
Girish Hakki <109576032+GirishHakki@users.noreply.github.com>
2025-12-07 08:35:30 +0530

    Update ansible.sh

--- ansible.sh
+++ ansible.sh
@@ -1,2 +1,20 @@
 amazon-linux-extras install ansible2 -y
 yum install python3 python-pip python-dlevel -y
+
+
+# If above commands are not working for Amazon Linux 2023 Kernel, use below commands
+
+# for ansible
+sudo dnf install ansible-core -y
+
+#verify
+ansible --version
+
+# for python
+yum install python3 python-pip python-dlevel -y
+
+#verify
+python3 --version
+pip3 --version
+
+
